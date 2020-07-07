831c1a5a8ec7e8c62f0e7b20536959f72138e013
fangchaolong <2925725541@qq.com>
2020-07-07 20:01:16 +0800

    Site updated: 2020-07-07 20:01:12

--- js/photo.js
+++ js/photo.js
@@ -1,32 +1,41 @@
 var photoobj = {
-    page: 1,
-    offset: 20,
+    // page: 1,
+    // offset: 20,
     init: function () {
         var that = this;
         console.log('ccc')
         $.getJSON("/photo/main.json", function (data) {
-            console.log(data)
-            that.render(that.page, data);
+            data[0].list.map(item => {
+                that.render(that.page, item);
+            })
 
             that.scroll(data);
         });
     },
 
     render: function (page, data) {
-        var begin = (page - 1) * this.offset;
-        var end = page * this.offset;
-        if (begin >= data.length) return;
-        var html, li = "";
-        for (var i = begin; i < end && i < data.length; i++) {
-            li += '<li><div class="img-box">' +
-                '<a class="img-bg" rel="example_group" href="https://github.com/fangchaolong/fangchaolong.github.io/blob/master/photo' + data[i] + '?raw=true"></a>' +
-                '<img data-src="https://github.com/fangchaolong/fangchaolong.github.io/blob/master/photo' + data[i] + '?raw=true" class="lazyload"/>' +
-                '</li>';
+        // var begin = (page - 1) * this.offset;
+        // var end = page * this.offset;
+        // if (begin >= data.length) return;
+        console.log(data)
+        var html, li = "<h1>" + data['name'] + "</h1>";
+        for (var i = 0; i < data['list'].length; i++) {
+            if (i === 0) {
+                li += '<div class="img-box">'
+            }
+
+            li += '<a class="img-bg" rel="example_group" href="/photo' + data['list'][i]['src'] + '?raw=true">' + 
+                '<img data-src="/photo' + data['list'][i]['src'] + '?raw=true" class="lazyload"/>' +
+                '</a>';
+
+            if (i === data['list'].length-1) {
+                li += '</div>';
+            }
         }
 
         $(".img-box-ul").append(li);
-        $(".img-box-ul").lazyload();
-        $("a[rel=example_group]").fancybox();
+        // $(".img-box-ul").lazyload();
+        // $("a[rel=example_group]").fancybox();
     },
 
     scroll: function (data) {
