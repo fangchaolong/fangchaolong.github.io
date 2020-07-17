294bc5a8ba8c6581a22f84424b10c57e1b73f481
fangchaolong <2925725541@qq.com>
2020-07-17 22:55:39 +0800

    Site updated: 2020-07-17 22:55:25

--- js/photo.js
+++ js/photo.js
@@ -13,27 +13,47 @@ var photoobj = {
             that.scroll(data[0]);
         });
     },
+    renderarr: function(data) {
+        var str = ''
+        for(var i = 0;i < data.length;i++) {
+            if (i === 0) {
+                str = '<div class="img_lie">'
+            }
 
+            str += '<a class="img-bg" rel="example_group" href="/photo' + data[i]['src'] + '?raw=true">' + 
+            '<img data-src="/photo' + data[i]['src'] + '?raw=true" class="lazyload"/>' +
+            '</a>';
+
+            if (i === data.length-1) {
+                str += '</div>';
+            }
+        }
+        return str
+    },
     render: function (page, data) {
         // var begin = (page - 1) * this.offset;
         // var end = page * this.offset;
         // if (begin >= data.length) return;
         console.log(data)
+        var arr1 = [],arr2 = [],arr3 = [],arr4 = [];
         var html, li = "<h1>" + data['name'] + "</h1>";
         for (var i = 0; i < data['list'].length; i++) {
-            if (i === 0) {
-                li += '<div class="img-box">'
-            }
-
-            li += '<a class="img-bg" rel="example_group" href="/photo' + data['list'][i]['src'] + '?raw=true">' + 
-                '<img data-src="/photo' + data['list'][i]['src'] + '?raw=true" class="lazyload"/>' +
-                '</a>';
-
-            if (i === data['list'].length-1) {
-                li += '</div>';
+            if (i%4 === 0) {
+                arr1.push(data['list'][i])
+            } else if (i%4 === 1) {
+                arr2.push(data['list'][i])
+            } else if (i%4 === 2) {
+                arr3.push(data['list'][i])
+            } else {
+                arr4.push(data['list'][i])
             }
         }
-
+        li += '<div class="img-box">'
+        li += this.renderarr(arr1)
+        li += this.renderarr(arr2)
+        li += this.renderarr(arr3)
+        li += this.renderarr(arr4)
+        li += '</div>'
         $(".img-box-ul").append(li);
         // $(".img-box-ul").lazyload();
         // $("a[rel=example_group]").fancybox();
